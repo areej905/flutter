09a216020b1b618adf8a934c0474a0b63975b36f
areej905 <areejshakir3@gmail.com>
2025-12-29 19:40:25 +0400

    Add files via upload

---
+++ UserAuthRepo.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:grocery_shop/data/repos/UserAuthRepo.dart';
+
+class UserAuthRepo {
+  Future<UserCredential> login(String email, String password) async {
+    return await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: email.trim(),
+      password: password.trim(),
+    );
+  }
+  Future<UserCredential> signup(String email, String password) async {
+    return await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: email.trim(),
+      password: password.trim(),
+
+    );
+  }
+  Future<void> resetPassword(String email) async {
+    if (email.isEmpty) {
+      throw FirebaseAuthException(
+        code: 'empty-email',
+        message: 'Please enter your email',
+      );
+    }
+    await FirebaseAuth.instance.sendPasswordResetEmail(email: email.trim());
+  }
+}
